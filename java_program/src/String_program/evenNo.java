ac1d07a099edbb77628a17ac3c4183162e23890a
manish23032022 <102226602+manish23032022@users.noreply.github.com>
2024-06-03 10:41:45 +0530

    derfgth

---
+++ java_program/src/String_program/evenNo.java
@@ -0,0 +1,16 @@
+package String_program;
+
+public class evenNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=8;
+		if(i%2==0) {
+			System.out.println("even no");
+		}
+		else {
+			System.out.println("odd no");
+		}
+	}
+
+}
